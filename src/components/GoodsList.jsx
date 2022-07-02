e31458f4e5aa08412135ca88802324018dfdec1b
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-07-02 17:34:34 +0300

    Вывод списка товаров

---
+++ src/components/GoodsList.jsx
@@ -0,0 +1,15 @@
+import { GoodsItem } from "./GoodsItem";
+
+export function GoodsList({goods = []}) {
+    if (!goods.length) {
+        return <h3>Nothing here</h3>
+    }
+
+    return (
+        <div className="goods">
+            {goods.map(item => (
+                <GoodsItem key={item.id} {...item} />
+            ))}
+        </div>
+    );
+}
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+
+import { GoodsList } from "./GoodsList";
+import { Preloader } from "./Preloader";
+
+import { API_KEY, API_URL } from '../config';
+
 export function Shop() {
+    const [goods, setGoods] = useState([]);
+    const [lodaing, setLoading] = useState(true);
+
+    useEffect(function getGoods() {
+        fetch(API_URL, {
+            headers: {
+                Authorization: API_KEY,
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                data.featured && setGoods(data.featured);
+                setLoading(false);
+            });
+    }, []);
+
     return (
         <main className="container content">
-            Shop
+            {
+                lodaing ? <Preloader /> : <GoodsList goods={goods} />
+            }
         </main>
     );
 }
