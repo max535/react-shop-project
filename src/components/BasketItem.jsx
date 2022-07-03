f06f230e18e19027bb76642212e4837756c91d32
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-07-03 14:24:12 +0300

    Добавляет функционал для изменение количества товаров в корзине

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -4,10 +4,20 @@ export function BasketItem({
     price,
     quanitity,
     removeFromBasket = Function.prototype,
+    incQuantity = Function.prototype,
+    decQuantity = Function.prototype,
 }) {
     return (
         <li className="collection-item">
-            {name} x{quanitity} = {price * quanitity} руб.
+            {name}
+            <i className="material-icons basket-quantity" onClick={() => decQuantity(id)}>
+                remove
+            </i>
+            x{quanitity}
+            <i className="material-icons basket-quantity" onClick={() => incQuantity(id)}>
+                add
+            </i>
+            = {price * quanitity} руб.
             <span
                 className="secondary-content"
                 onClick={() => removeFromBasket(id)}
