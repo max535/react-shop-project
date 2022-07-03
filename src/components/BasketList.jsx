f8979b4d85b1ed979eb32f5fa2f744ecd6e65425
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-07-03 13:44:51 +0300

    Добавляет функцию для удаления товаров из корзины

--- src/components/BasketList.jsx
+++ src/components/BasketList.jsx
@@ -3,16 +3,23 @@ import { BasketItem } from "./BasketItem";
 export function BasketList({
     order = [],
     handleBasketShow = Function.prototype,
+    removeFromBasket = Function.prototype,
 }) {
     const totalPrice = order.reduce((sum, el) => {
-        return sum + el.price * el.quantity
+        return sum + el.price * el.quanitity;
     }, 0);
 
     return (
         <ul className="collection basket-list">
             <li className="collection-item active">Корзина</li>
             {order.length ? (
-                order.map(item => <BasketItem key={item.id} {...item} />)
+                order.map(item => (
+                    <BasketItem 
+                        key={item.id}
+                        removeFromBasket={removeFromBasket}
+                        {...item}
+                    />
+                ))
             ) : (
                 <li className="collection-item">
                     Корзина пуста
